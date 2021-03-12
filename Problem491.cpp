921ab537aa3ce37a372151005e57bbd8565d6fc0
Kaustubh Sable <kaustubhsable05@gmail.com>
2021-03-11 22:50:45 -0800

    Adding optimized solution

--- Problem491.cpp
+++ Problem491.cpp
@@ -10,10 +10,11 @@
 using namespace std;
 
 bool isPalindrome(int);
+bool isPalindrome_optimized(int);
 
 int main() {
-  int num = -121;
-  if(isPalindrome(num))
+  int num = 121;
+  if(isPalindrome_optimized(num))
     cout<<"The number is a palindrome.";
   else 
     cout<<"The number is not a palindrome.";
@@ -22,36 +23,49 @@ int main() {
 }
 
 bool isPalindrome(int num) {
-    if(num<0)
-        return false;
-    
-    stack<int> s;
-    int n = num;
-    while(n>0) {
-        s.push(n%10);
-        n/=10;
-    }
-    
-    int n_digits = s.size();
-    
-    while(!s.empty())
-        s.pop();
-    
-    n = num;
-    for(int i=0;i<n_digits/2;i++) {
-        s.push(n%10);
-        n/=10;
-    }
-    
-    if(n_digits%2!=0)
-        n/=10;
-    
-    for(int i=0;i<n_digits/2;i++) {
-        if(s.top()!=n%10)
-            return false;
-        s.pop();
-        n/=10;
-    }
-    
-    return true;
+  if(num<0)
+      return false;
+  
+  stack<int> s;
+  int n = num;
+  while(n>0) {
+      s.push(n%10);
+      n/=10;
+  }
+  
+  int n_digits = s.size();
+  
+  while(!s.empty())
+      s.pop();
+  
+  n = num;
+  for(int i=0;i<n_digits/2;i++) {
+      s.push(n%10);
+      n/=10;
+  }
+  
+  if(n_digits%2!=0)
+      n/=10;
+  
+  for(int i=0;i<n_digits/2;i++) {
+      if(s.top()!=n%10)
+          return false;
+      s.pop();
+      n/=10;
+  }
+  
+  return true;
+}
+
+bool isPalindrome_optimized(int num) {
+  if(num<0 || (num%10==0 && num!=0))
+      return false;
+  
+  int reverse = 0;
+  while (num>reverse) {
+      reverse = reverse*10 + num%10;
+      num /= 10;
+  }
+  
+  return num==reverse || num==reverse/10;
 }
