a57bad2da3a1b2ea0fb61e7a9a8dc0d243db7acb
Kaustubh Sable <kaustubhsable05@gmail.com>
2021-03-11 19:43:49 -0800

    Adding solution for DailyCodingProblem of 3/11/2021

---
+++ Problem491.cpp
@@ -0,0 +1,57 @@
+/**
+ * Problem Statement:
+ * Write a program that checks whether an integer is a palindrome. For example,
+ * 121 is a palindrome, as well as 888. 678 is not a palindrome. Do not convert
+ * the integer into a string.
+**/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isPalindrome(int);
+
+int main() {
+  int num = -121;
+  if(isPalindrome(num))
+    cout<<"The number is a palindrome.";
+  else 
+    cout<<"The number is not a palindrome.";
+
+  return 0;
+}
+
+bool isPalindrome(int num) {
+    if(num<0)
+        return false;
+    
+    stack<int> s;
+    int n = num;
+    while(n>0) {
+        s.push(n%10);
+        n/=10;
+    }
+    
+    int n_digits = s.size();
+    
+    while(!s.empty())
+        s.pop();
+    
+    n = num;
+    for(int i=0;i<n_digits/2;i++) {
+        s.push(n%10);
+        n/=10;
+    }
+    
+    if(n_digits%2!=0)
+        n/=10;
+    
+    for(int i=0;i<n_digits/2;i++) {
+        if(s.top()!=n%10)
+            return false;
+        s.pop();
+        n/=10;
+    }
+    
+    return true;
+}
