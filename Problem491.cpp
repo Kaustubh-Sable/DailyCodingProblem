123b64e8ff10863f46e39151bcfeac9ef39e5f53
Kaustubh Sable <kaustubhsable05@gmail.com>
2021-03-13 14:30:37 -0800

    Updating comment

--- Problem491.cpp
+++ Problem491.cpp
@@ -1,4 +1,5 @@
 /**
+ * This problem was asked by Palantir.
  * Problem Statement:
  * Write a program that checks whether an integer is a palindrome. For example,
  * 121 is a palindrome, as well as 888. 678 is not a palindrome. Do not convert
